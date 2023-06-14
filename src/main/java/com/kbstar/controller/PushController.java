a508a77dadc8c9504df3c7367bde3b7e8fdb0c3d
David Byun <byundavid@naver.com>
2023-06-14 11:32:22 +0900

    푸시 변경

--- src/main/java/com/kbstar/controller/PushController.java
+++ src/main/java/com/kbstar/controller/PushController.java
@@ -4,17 +4,18 @@ import com.kbstar.dto.FCMNotificationRequestDto;
 import com.kbstar.service.FCMNotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @Slf4j
 @RequiredArgsConstructor
 public class PushController {
     private final FCMNotificationService fcmNotificationService;
 
     @PostMapping("/notification")
-    public void sendNotificationByToken(FCMNotificationRequestDto requestDto) {
+    public String sendNotificationByToken(FCMNotificationRequestDto requestDto) {
         fcmNotificationService.sendNotificationByToken(requestDto);
+        return "redirect:/";
     }
 }
